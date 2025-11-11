0532d2d46f33853152dfd8012d5c28853615cb22
Štěpánka Pokorná <steankapokorna1@seznam.cz>
2025-11-11 14:42:10 +0100

    Dokončení příkladu

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const max3 = (number1, number2, number3) => {
     if(number1 > number2 && number1 > number3){
-        return number1;
-    } else if (number2 > number1 && number2 > number3){
-        return number2;
-    } else{
-        return number3;
-    }    
+    return number1;
+    } if (number2 > number1 && number2 > number3){
+    return number2;
+    }
+    return number3;
+
 };
 
-document.body.innerHTML += max3(22,500,80);
+document.body.innerHTML += max3(11,150,80);
